b0e14edaaa6e46967f56a6f212603fda4a12ce76
Maria Roberta <mariarobertaprado@outlook.com>
2021-07-28 21:58:14 -0300

    Funções com retorno de tabela

---
+++ Functions/Função com retorno de tabela.sql
@@ -0,0 +1,30 @@
+--Funções com retorno de tabela
+--Função que retorna a diferença em minutos de duas datas diferentes
+CREATE FUNCTION FN_DATA_MINUTOS(@Min INT, @InicialDate DATETIME, @FinalDate DATETIME)
+RETURNS @TABLE TABLE(DATA DATETIME)
+AS
+BEGIN
+
+	WHILE @InicialDate <= @FinalDate
+	BEGIN
+		INSERT INTO @TABLE VALUES (@InicialDate)
+		SET @InicialDate = DATEADD(MINUTE, @Min, @InicialDate)
+	END
+	RETURN 
+END
+
+SELECT * FROM FN_DATA_MINUTOS (60, '28-07-2021 19:00:00.000', GETDATE())
+
+
+--Criando uma função que retorna as locações a partir de uma data
+CREATE FUNCTION locadosEM(@DATA DATETIME)
+RETURNS TABLE
+AS 
+RETURN(
+		SELECT * FROM locacao 
+		WHERE dataLocacao >= @DATA
+		)
+
+SELECT * FROM locadosEM('28-01-2019 19:00:00.000')
+
+
