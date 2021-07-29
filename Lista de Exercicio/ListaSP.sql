4f35e3bccaa3332439cd9be980c65d5f9814414e
Maria Roberta <mariarobertaprado@outlook.com>
2021-07-28 23:57:44 -0300

    Teste

--- Lista de Exercicio/ListaSP.sql
+++ Lista de Exercicio/ListaSP.sql
@@ -1,7 +1,7 @@
 --Utilizando o BD LOCADORA crie os seguintes Stored Procedures:
 
 /*(1) Exiba a quantidade total de locações de um determinado filme. (Exibir o id, nome do filme
-e quantidade de locações)*/SELECT DISTINCT	fi.id, fi.descricao, COUNT(fit.filmeId) 'Locações'FROM	locacao lJOIn	fita fit ON fit.id = l.fitaIdJOIN 	filme fi ON fi.id = fit.filmeIdGROUP BY		fi.descricao, fi.id/*(02) Exiba todas as locações efetuadas por um determinando cliente. (Exibir o id, nome do
+e quantidade de locações)*/SELECT DISTINCT	fi.id, fi.descricao, COUNT(fit.filmeId) 'Locações'FROM	locacao lJOIn	fita fit ON fit.id = l.fitaIdJOIN 	filme fi ON fi.id = fit.filmeIdGROUP BYfi.descricao, fi.id/*(02) Exiba todas as locações efetuadas por um determinando cliente. (Exibir o id, nome do
 cliente e quantidade de locações)*/SELECT DISTINCT c.id, c.nome, COUNT(l.fitaId) as 'Locações'FROM	locacao lJOIN	cliente c ON c.id = l.clienteIdGROUP BY	c.id, c.nome/*
 (03) Calcule o valor total de locações para as categorias de filme com base nas locações do
 mês/ano (mês e ano serão parâmetros IN)*/SELECT DISTINCT 	cat.descricao, SUM(fi.valor), COUNT(fit.filmeId) 'Locações'FROM	locacao lJOIN 	fita fit ON fit.id = l.fitaIdJOIN 	filme fi ON fi.id = fit.filmeIdJOIN 	categoria cat ON cat.id = fi.categoriaIdGROUP BY	cat.descricao/*
