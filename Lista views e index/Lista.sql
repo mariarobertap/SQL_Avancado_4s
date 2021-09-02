2e476e3996804a030b7373591b2906d4f042f24f
Maria Roberta <mariarobertaprado@outlook.com>
2021-09-01 22:20:20 -0300

    Falta as views

--- Lista views e index/Lista.sql
+++ Lista views e index/Lista.sql
@@ -14,9 +14,24 @@ Order status:
 4 = Completed
 */
 
+
+
+SELECT 
+	count(o.order_id), s.store_name, st.first_name
+FROM 
+	sales.orders o
+JOIN
+	sales.stores s on o.store_id = s.store_id
+JOIN
+	sales.staffs st on s.store_id = st.store_id
+WHERE
+	o.order_status = 4
+GROUP BY
+	s.store_name, st.first_name
+
 /*
-2) Crie uma view para exibir o número de orders (pedidos) por ano, mês 
-(order_date) e order status (situação). O número de orders deverá ser
+2) Crie uma view para exibir o [número de orders (pedidos) por ano, mês 
+(order_date) e order status (situação)]. O número de orders deverá ser
 apresentado em colunas representadas por cada order status (situação).
 Order status: 
 1 = Pending; 
@@ -24,6 +39,23 @@ Order status:
 3 = Rejected; 
 4 = Completed
 */
+
+select * from sales.orders
+
+SELECT 
+	count(o.order_id), o.order_status, Month(order_date), year(order_date)
+FROM 
+	sales.orders o
+JOIN
+	sales.stores s on o.store_id = s.store_id
+JOIN
+	sales.staffs st on s.store_id = st.store_id
+WHERE
+	o.order_status = 4
+GROUP BY
+	 Month(order_date), year(order_date),  o.order_status
+
+
 
 /*
 3) Crie uma view para exibir a revenue (receita) e a revenue accumulated 
@@ -34,7 +66,7 @@ Order status:
 4) Crie uma view para exibir o volume orders completed (volume de pedidos concluídos)
 e a revenue (receita) por products (produtos) e model year (modelo do ano).
 */
-
+select * from production.products
 /*
 5) Crie uma view para exibir o volume orders completed (volume de pedidos concluídos)
 e a revenue (receita) por brands (marcas).
