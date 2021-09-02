eced35da7ed84c35e4fe84bf15b2c4ec6d5600a8
Maria Roberta <mariarobertaprado@outlook.com>
2021-09-01 22:34:56 -0300

    Exercicio de acumulado

--- Lista views e index/Lista.sql
+++ Lista views e index/Lista.sql
@@ -62,6 +62,29 @@ GROUP BY
 (receita acumulada) de orders completed (pedidos concluídos) por ano.
 */
 
+
+	SELECT
+		YEAR(o.order_date) as 'Ano',
+		FORMAT(SUM(oi.list_price * oi.quantity), 'C', 'PT-BR') AS 'Faturamento',
+		'Faturamento Acumulado' = (
+				SELECT 
+					FORMAT(SUM(oi2.list_price * oi2.quantity), 'C', 'PT-BR') AS 'Montante Total'
+					FROM 
+						sales.order_items oi2
+					JOIN
+						sales.orders o2 on oi2.order_id = o2.order_id
+					WHERE
+					o2.order_status = 4
+				AND  YEAR(o2.order_date) <= YEAR(o.order_date)
+			   )
+		FROM 
+			sales.order_items oi
+		JOIN
+			sales.orders o on oi.order_id = o.order_id
+		WHERE
+			o.order_status = 4
+		GROUP BY
+			YEAR(o.order_date)
 /*
 4) Crie uma view para exibir o volume orders completed (volume de pedidos concluídos)
 e a revenue (receita) por products (produtos) e model year (modelo do ano).
