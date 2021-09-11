19fe197a0226e702e3662dfc8d376b847329edfc
Maria Roberta <mariarobertaprado@outlook.com>
2021-09-10 23:05:39 -0300

    Desafio completo

--- Lista views e index/Lista.sql
+++ Lista views e index/Lista.sql
@@ -231,42 +231,41 @@ de cada customer (cliente).
 a revenue (receita), o discounts (total de descontos) e a percentage discounts 
 (percentual de descontos) por ano e trimestre.
 */
-
-select 
-	f.volume,
-	FORMAT(f.amount,  'C', 'PT-BR'),
-	FORMAT(f.disc,   'C', 'PT-BR'),
-	cast(Round((f.disc / f.amount *100), 2) as decimal(18,2)),
-	f.Year,
-	f.Trimestre
+CREATE VIEW sales.ivw_getPercentageOFFByquarter
+WITH SCHEMABINDING
+AS
+SELECT 
+	tab.volumn as 'Volume' ,
+	FORMAT(tab.amount,  'C', 'PT-BR') 'Total amount (revenue)',
+	FORMAT(tab.disc,   'C', 'PT-BR') 'Total discounts',
+	CONVERT(VARCHAR(6),CAST(ROUND((tab.disc / tab.amount *100), 2) AS DECIMAL(18,2))) + '%' 'Percent OFF',
+	tab.year AS 'Year',
+	tab.trimestre AS 'Quarter'
 	
 FROM(
 	SELECT 
-		count(distinct o.order_id) as volume,
-		SUM(s.list_price * s.quantity) as amount,
-		SUM(((s.list_price * s.quantity) * s.discount)) as disc,
-		YEAR(o.order_date) as Year,
-		DATEPART(QUARTER, o.order_date) as Trimestre
+		count(DISTINCT o.order_id) AS volumn,
+		SUM((s.quantity * s.list_price) * (1 - s.discount)) AS amount,
+		SUM(((s.list_price * s.quantity)  * s.discount)) AS disc,
+		YEAR(o.order_date) AS year,
+		DATEPART(QUARTER, o.order_date) as trimestre
 	FROM 
 		sales.orders o
 	JOIN
-		sales.order_items s on o.order_id = s.order_id
+		sales.order_items s ON o.order_id = s.order_id
 	JOIN
-		production.products p on s.product_id = p.product_id
+		production.products p ON s.product_id = p.product_id
 	WHERE
 		o.order_status = 4 
 	GROUP BY
 		YEAR(o.order_date),
 		DATEPART(QUARTER, o.order_date)
 
-)  AS f
-	ORDER BY 
-		f.Year,
-		f.Trimestre
+)  AS tab
+ORDER BY 
+	tab.year,
+	tab.trimestre
 
 
 
-
-
-		
-	
+	SELECT * FROM sales.order_items
