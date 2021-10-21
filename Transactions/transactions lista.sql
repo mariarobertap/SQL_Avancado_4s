ad6d750bfae881772d381f297abea1a132a43222
Maria Roberta <mariarobertaprado@outlook.com>
2021-10-20 22:35:09 -0300

    Lista

---
+++ Transactions/transactions lista.sql
@@ -0,0 +1,14 @@
+USE LOJA;
+GO
+
+/* Transação
+1. Crie uma transação para atualizar o valor unitário em 10% de todos os produtos da base.
+2. Crie uma transação para atualizar o salário dos vendedores respeitando as suas faixas de comissão conforme segue: (A: 5% - B: 10% - C: 15% - D: 20%)
+3. Crie uma instrução SQL para atualizar os clientes que residem no estado do PR para SP, reverta a operação.
+4. Crie uma transação para concretizar uma ordem de pedido e item do pedido.
+5. Com base no exercício anterior, tente inserir um produto na tabela item pedido que não exista na tabela de produto.6. Crie uma sentença sql com uma transação para excluir todos os dados de pedidos.
+*/
+
+/* Locks, Blocks e DeadLocks
+1. Utilizando o BD Loja, crie um ambiente simulado para demonstrar os mecanismos do SGBD (SQL Server) para gerenciamento de LOCK, BLOCK e DEADLOCK.
+*/
